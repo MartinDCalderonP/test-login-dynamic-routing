a5527b2f7b717df0b8638b156e8049c029264088
Martín <martincalderon92@outlook.com>
2023-05-23 14:13:28 -0300

    Add more styles

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -20,13 +20,13 @@ const Login = () => {
     <form className="flex flex-col space-y-4">
       {fields.map((field) => (
         <input
-          className="border border-gray-300 rounded-md p-2"
+          className="border border-gray-300 rounded-md p-2 text-gray-700"
           key={field.name}
           {...field}
         />
       ))}
       <button
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded transition-all"
         type="submit"
       >
         Login
