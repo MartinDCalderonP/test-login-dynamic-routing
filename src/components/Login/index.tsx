646bce1136289fa9a9ffade2fbe7b1841d84c898
Martín <martincalderon92@outlook.com>
2023-05-24 10:09:01 -0300

    Add logic to redirect after login

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,4 +1,7 @@
-import { ChangeEvent, useState } from "react"
+import { ChangeEvent, FormEvent, useEffect, useState } from "react"
+import { useRouter } from "next/router"
+import { useUser } from "@/context/UserContext"
+import { actionTypes } from "@/context/reducer"
 
 const fields = [
   {
@@ -18,6 +21,9 @@ const fields = [
 ]
 
 const Login = () => {
+  const router = useRouter()
+  const { state, dispatch } = useUser()
+
   const [user, setUser] = useState({
     email: "",
     password: ""
@@ -28,8 +34,23 @@ const Login = () => {
     setUser((prev) => ({ ...prev, [name]: value }))
   }
 
+  const handleFormSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    dispatch({ type: actionTypes.SET_USER, payload: user })
+  }
+
+  useEffect(() => {
+    if (!state.user) return
+
+    router.push(`/${state.user.email.split("@")[0]}`)
+  }, [router, state.user])
+
   return (
-    <form className="flex flex-col space-y-4">
+    <form
+      className="flex flex-col space-y-4"
+      onSubmit={handleFormSubmit}
+      noValidate
+    >
       {fields.map((field) => (
         <input
           className="border border-gray-300 rounded-md p-2 text-gray-700"
